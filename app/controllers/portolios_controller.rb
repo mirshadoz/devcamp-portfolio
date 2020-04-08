dee2a9e501af6dd5ab5ff6ef6d516eae35d60e49
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-08 20:04:24 +0600

    Created seeds file for sample data. And changed table for the portfolios

--- app/controllers/portolios_controller.rb
+++
@@ -1,2 +0,0 @@
-class PortoliosController < ApplicationController
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_082832) do
+ActiveRecord::Schema.define(version: 2020_04_08_135351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_082832) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "portolios", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.text "body"
