997fc9eee51d078b074e2ddfb8dba8410c449342
Mirshad Oz <mirshadoz@Mirshads-MacBook-Pro.local>
2020-04-13 23:11:53 +0600

    Implemented Destroy action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -49,5 +49,17 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    # Perfrom a lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    # Destroy or delete the record
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
+    end
+  end
+
 
 end
